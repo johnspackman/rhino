be5b1c9bca4ee8e41643a74ba010c28fbd8a1632
John Spackman <john.spackman@zenesis.com>
2015-09-09 12:37:52 +0100

    test script

---
+++ testsrc/org/mozilla/javascript/tests/json/JsonStringifyTest.java
@@ -0,0 +1,35 @@
+package org.mozilla.javascript.tests.json;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.json.JsonParser;
+import org.mozilla.javascript.json.JsonParser.ParseException;
+
+public class JsonStringifyTest {
+
+    private Context cx;
+    private Scriptable scope;
+    
+    @Before
+    public void setUp() {
+        cx = Context.enter();
+        scope = cx.initStandardObjects();
+    }
+
+    @After
+    public void tearDown() {
+        Context.exit();
+    }
+
+    @Test
+    public void writeEnum() throws Exception {
+		ScriptableObject.putProperty(scope, "myArray", Context.javaToJS(new Object[] { MYENUM.ALPHA, MYENUM.BRAVO, MYENUM.CHARLIE }, scope));
+		cx.evaluateString(scope, "JSON.stringify(myArray)", "[inline command]", 1, null);
+    }
+    
+    public enum MYENUM { ALPHA, BRAVO, CHARLIE };
+}
