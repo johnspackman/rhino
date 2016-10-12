b0d6d3ce2046da63b3a663ee7caf6f9bc232469b
John Spackman <john.spackman@zenesis.com>
2016-10-12 08:21:46 +0100

    unit test for jdk ArrayList.forEach

---
+++ testsrc/org/mozilla/javascript/tests/ArrayListConsumerTest.java
@@ -0,0 +1,33 @@
+package org.mozilla.javascript.tests;
+
+import java.util.ArrayList;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextAction;
+import org.mozilla.javascript.ScriptableObject;
+
+import junit.framework.TestCase;
+
+public class ArrayListConsumerTest extends TestCase
+{
+    public void testIt()
+    {
+        final ArrayList<String> arr = new ArrayList<>();
+        arr.add("alpha");
+        arr.add("bravo");
+        arr.add("charlie");
+        final String script = "var str = ''; arr.forEach(function(elem) { str += elem; }); str;";
+
+        final ContextAction action = new ContextAction() {
+            public Object run(final Context _cx)
+            {
+                final ScriptableObject scope = _cx.initStandardObjects();
+                ScriptableObject.putProperty(scope, "arr", Context.javaToJS(arr, scope));
+                final Object result = _cx.evaluateString(scope, script, "test script", 0, null);
+                assertEquals("alphabravocharlie", Context.toString(result));
+                return null;
+            }
+        };
+        Utils.runWithAllOptimizationLevels(action);
+    }
+}
