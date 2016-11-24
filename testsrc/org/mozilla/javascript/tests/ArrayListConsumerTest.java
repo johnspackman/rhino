a234659591536ae85c466b44cfc726e1a319cb28
John Spackman <john.spackman@zenesis.com>
2016-11-24 10:03:30 +0000

    added test

--- testsrc/org/mozilla/javascript/tests/ArrayListConsumerTest.java
+++ testsrc/org/mozilla/javascript/tests/ArrayListConsumerTest.java
@@ -8,10 +8,9 @@ import org.mozilla.javascript.ScriptableObject;
 
 import junit.framework.TestCase;
 
-public class ArrayListConsumerTest extends TestCase
-{
-    public void testIt()
-    {
+public class ArrayListConsumerTest extends TestCase {
+    
+    public void testNativeArray() {
         final ArrayList<String> arr = new ArrayList<>();
         arr.add("alpha");
         arr.add("bravo");
@@ -19,10 +18,28 @@ public class ArrayListConsumerTest extends TestCase
         final String script = "var str = ''; arr.forEach(function(elem) { str += elem; }); str;";
 
         final ContextAction action = new ContextAction() {
-            public Object run(final Context _cx)
-            {
+            @Override
+            public Object run(final Context _cx) {
                 final ScriptableObject scope = _cx.initStandardObjects();
                 ScriptableObject.putProperty(scope, "arr", Context.javaToJS(arr, scope));
+                final Object result = _cx.evaluateString(scope, script, "test script", 0, null);
+                assertEquals("alphabravocharlie", Context.toString(result));
+                return null;
+            }
+        };
+        Utils.runWithAllOptimizationLevels(action);
+    }
+    
+    public void testJsArray() {
+        final String script = "var str = '';\n" +
+                "var arr = [ 'alpha', 'bravo', 'charlie' ];\n" +
+                "arr.forEach(function(elem) { str += elem; });\n" +
+                "str;";
+
+        final ContextAction action = new ContextAction() {
+            @Override
+            public Object run(final Context _cx) {
+                final ScriptableObject scope = _cx.initStandardObjects();
                 final Object result = _cx.evaluateString(scope, script, "test script", 0, null);
                 assertEquals("alphabravocharlie", Context.toString(result));
                 return null;
